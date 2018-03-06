c84d15ce3b747225aabd9d4998c9efa97ef44b70
markic997 <36863573+markic997@users.noreply.github.com>
2018-03-06 19:46:36 +0100

    Add files via upload

--- includes/db_config.php
+++ includes/db_config.php
@@ -1,15 +1,9 @@
 <?php
 
-$db['db_host']="localhost";
-$db['db_user']="root";
-$db['db_pass']="";
-$db['db_name']="cms";
-
-
-foreach ($db as $key => $value){
-    define(strtoupper($key), $value);
-
-}
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASS","");
+define("DB_NAME","myhobby-test");
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 if($connection){
