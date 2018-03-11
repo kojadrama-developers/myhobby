4f76096b59004734b5fbbc392154c984729239d7
markic997 <milenkovicmarko997@gmail.com>
2018-03-11 13:01:17 +0100

    Admin

--- RegLog/home.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();
-    echo $_SESSION['username'];
-    echo "<a href='logout.php'>Logout</a>"
-?>
--- RegLog/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-unset($_SESSION['username']);
-$_SESSION['message']="You are logged out";
-header("location: login.php");
-?>
