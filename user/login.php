47120487623fa98fc78e2034ec12f4d2a65552a2
markic997 <milenkovicmarko997@gmail.com>
2018-05-29 16:32:47 +0200

    Prso sam, popravio jos jednu gresku za login, islo je na login.php kad se unese nepostojeci email, sad vise ne

--- user/login.php
+++ user/login.php
@@ -7,7 +7,16 @@ if($login->is_loggedin()!="")
 {
     $login->redirect('../index.php');
 }
-if($_POST['email']=="" or ($_POST['password'])==""){
+$stmt=$login->runQuery("SELECT email FROM `myhobby`.users");
+$stmt->execute(array(':email'=>$email));
+$row=$stmt->fetch(PDO::FETCH_ASSOC);
+
+if($row['email']!=$email)
+{
+    $error[]="Sorry, email already taken!";
+    $login->redirect("../index.php");
+}
+else if($_POST['email']=="" or ($_POST['password'])==""){
     $error="All fields are required";
     echo $error;
     $login->redirect("../index.php");
