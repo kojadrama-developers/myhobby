d11fa5324f389d168c942a2fb8bdbba314c91d8e
markic997 <milenkovicmarko997@gmail.com>
2018-06-03 14:03:39 +0200

    Dodati komentari...

--- user/photo.php
+++ user/photo.php
@@ -15,25 +15,30 @@
         $image=new USER();
         $user_id=$_SESSION['user_session'];
 
+        //finds in database if user has a picture
         $stmt=$image->runQuery("SELECT * FROM image WHERE user_id='$user_id'");
         $stmt->execute();
         $row=$stmt->fetch(PDO::FETCH_ASSOC);
 
+        //if button upload is pressed
         if(isset($_POST['btn_upload'])){
+            //receive file from form and take its name and contents
             $name=$_FILES['photo']['name'];
             $type=$_FILES['photo']['type'];
             $data=file_get_contents($_FILES['photo']['tmp_name']);
-
+            
+            //if there is no file redirect
             if($name==''){
                 $image->redirect('photo.php');
             }
+            //if user has a picture change it
             else if($row['user_id']==$user_id){
                 $image->changeImage($data,$name);
             }
+            //if user doesn't have a picture upload picture
             else{
                 $image->imageUpload($data,$name);
             }
-            
         }
     ?>
     <form method="POST" enctype="multipart/form-data">
@@ -43,6 +48,7 @@
 </body>
 </html>
 <?php 
+    //show picture for logged in user
     $stmt=$image->runQuery("SELECT * FROM image WHERE user_id='$user_id'");
     $stmt->execute();
     while($row=$stmt->fetch(PDO::FETCH_BOTH)){
